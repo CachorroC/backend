9634eb4a4bf60e1453b7fa999448c7cc0bf1c14e
CachorroC <juankpato87@gmail.com>
2023-05-07 14:12:38 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Por favor ingrese el mensaje del commit para sus cambios. Las líneas que comiencen
     con '' serán ignoradas, y un mensaje vacío aborta el commit.
    
     En la rama master
     Tu rama está adelantada a 'origin/master' por 2 commits.
       (usa "git push" para publicar tus commits locales)
    
     Cambios a ser confirmados:mm
    	nuevo archivo:  out/Untitled-1.d.ts
    	nuevo archivo:  out/Untitled-1.js
    	nuevo archivo:  out/Untitled-1.js.map
    	borrado:        out/data/ConsultaNumeroRadicacion.procesos.json
    	borrado:        out/data/cleanProcesos.d.ts
    	borrado:        out/data/cleanProcesos.js
    	modificado:     out/data/procesos.d.ts
    	modificado:     out/data/procesos.js
    	modificado:     out/data/procesos.js.map
    	nuevo archivo:  out/json/llaves.json
    	borrado:        out/middleware/cleanProcesos.fixProcesos.json
    	modificado:     out/middleware/fetchProcesos.d.ts
    	modificado:     out/middleware/fetchProcesos.js
    	modificado:     out/middleware/fetchProcesos.js.map
    	modificado:     out/middleware/fixProcesos.d.ts
    	modificado:     out/middleware/fixProcesos.js
    	modificado:     out/middleware/fixProcesos.js.map
    	modificado:     out/middleware/polishProcesos.d.ts
    	modificado:     out/middleware/polishProcesos.js
    	modificado:     out/middleware/polishProcesos.js.map
    	borrado:        out/middleware/rows.fetchProcesos.json
    	modificado:     package.json
    	borrado:        quokka-typescript-import-sample
    	nuevo archivo:  src/Untitled-1.d.ts
    	nuevo archivo:  src/Untitled-1.js
    	nuevo archivo:  src/Untitled-1.js.map
    	modificado:     src/Untitled-1.ts
    	modificado:     src/data/ConsultaNumeroRadicacion.procesos.json
    	nuevo archivo:  src/data/error.procesos.json
    	modificado:     src/data/fulfilledFetch.procesos.json
    	modificado:     src/data/procesos.ts
    	nuevo archivo:  src/json/llaves.json
    	modificado:     src/middleware/Procesos.polishProcesos.json
    	modificado:     src/middleware/fixProcesos.ts
    	modificado:     src/middleware/polishProcesos.ts
    	modificado:     tsconfig.json

--- out/middleware/fetchProcesos.d.ts
+++ out/middleware/fetchProcesos.d.ts
@@ -1,3 +1,2 @@
-import { intConsultaNumeroRadicacion } from "#@/interface/procesos.js";
 export declare function fetchProcesoRama(llaveProceso: string): Promise<intConsultaNumeroRadicacion>;
 export declare const getProcesosRama: void;
